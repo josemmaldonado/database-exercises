de279d1be456c9454711987f98d61406afe950d6
MacOS <josemmaldonado50@gmail.com>
2021-11-18 15:26:25 -0600

    :where, order_by

---
+++ order_by_exercises.sql
@@ -0,0 +1,7 @@
+use employees;
+
+# SELECT first_name
+# FROM employees
+# where first_name IN ('Irena','Vidya','Maya')
+# ORDER BY first_name;
+
---
+++ where_exercises.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+# SELECT first_name
+# FROM employees
+# where first_name IN ('Irena','Vidya','Maya');
+
+# SELECT last_name
+# FROM employees
+# where last_name like 'E%';
+
+# SELECT last_name
+# FROM employees
+# WHERE last_name like ('%q%');
+
+
+#
+# SELECT first_name
+# FROM employees
+# where first_name OR ('Irena','Vidya','Maya')
+# AND (gender = 'M');
+#
+# SELECT last_name
+# FROM employees
+# WHERE last_name like '%E'
+# AND last_name like 'E%';
+
+# SELECT last_name
+# FROM employees
+# WHERE last_name ='q';
